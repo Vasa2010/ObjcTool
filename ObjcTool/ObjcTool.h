289c6b25988223c8aa98852cd149765e8040f63c
Pierluigi_MacBookPro <vasapolli.pierluigi@gmail.com>
2021-10-24 16:55:04 +0300

    Initial Commit

---
+++ ObjcTool/ObjcTool.h
@@ -0,0 +1,18 @@
+//
+//  ObjcTool.h
+//  ObjcTool
+//
+//  Created by iwaxT on 24/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjcTool.
+FOUNDATION_EXPORT double ObjcToolVersionNumber;
+
+//! Project version string for ObjcTool.
+FOUNDATION_EXPORT const unsigned char ObjcToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcTool/PublicHeader.h>
+
+
